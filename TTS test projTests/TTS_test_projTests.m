c3a0687cfe6d370497516bac485e94888a1f033b
Rong <rong@rongdemac-mini.local>
2018-04-27 15:27:30 +0800

    Initial Commit

---
+++ TTS test projTests/TTS_test_projTests.m
@@ -0,0 +1,39 @@
+//
+//  TTS_test_projTests.m
+//  TTS test projTests
+//
+//  Created by Rong on 2018/4/27.
+//  Copyright © 2018年 Rong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TTS_test_projTests : XCTestCase
+
+@end
+
+@implementation TTS_test_projTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
